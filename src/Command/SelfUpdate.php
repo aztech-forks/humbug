5f531279b68f670e5acd398fe42818fc495b7865
Padraic Brady <padraic.brady@gmail.com>
2015-02-21 11:51:11 +0000

    Set stage for self updates

---
+++ src/Command/SelfUpdate.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Humbug
+ *
+ * @category   Humbug
+ * @package    Humbug
+ * @copyright  Copyright (c) 2015 Pádraic Brady (http://blog.astrumfutura.com)
+ * @license    https://github.com/padraic/humbug/blob/master/LICENSE New BSD License
+ */
+
+namespace Humbug\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Finder\Finder;
+
+class SelfUpdate extends Command
+{
+
+    /**
+     * Execute the command.
+     *
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('done');
+    }
+
+    protected function configure()
+    {
+        $this
+            ->setName('self-update')
+            ->setDescription('Update humbug.phar to current version')
+        ;
+    }
+}
